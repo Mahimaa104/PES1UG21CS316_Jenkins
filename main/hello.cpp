fe5b9279f41d43daaed9c4837e8adcd1b470c34d
Mahimaa <93146344+Mahimaa104@users.noreply.github.com>
2024-03-07 18:57:20 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "I have successfully built and run\n";
   cout << "Cloud Computing Lab\n";
-  cout << "Performing Task 3\n";
   cout << "Webhook\n";
   return 0;
 }
